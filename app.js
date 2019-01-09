7fd0836c6e2a7c8c6f7fb35583c3a8b9926d57ff
Myles Y <39877233+mylesz123@users.noreply.github.com>
2019-01-09 16:15:36 -0500

    need to make project.pug functional

--- app.js
+++ app.js
@@ -1,52 +1,50 @@
 //project requires express, passed in as parameter
 const express = require('express');
-//require data from json file to fill in templates in project.pug file
-const data = require('./data.json')
-//creating new instance of express
 const app = express();
-//nodemon so i dont have to keep going in the terminal 
-const nodemon = require('nodemon')
+//require data from json file to fill in templates in project.pug file
+const data = require('./data.json');
+//nodemon so i dont have to keep going in the terminal
+//const nodemon = require('nodemon');
 
-//set(settings) view engine for app to use pug templates
+//app.settings view engine for app to use pug templates
 app.set('view engine', 'pug');
 
-//start public server to serve static files like style sheets
-app.use('/static', express.static('public'))
+//.use public server to use static files (images, css)
+app.use('/static', express.static('public'));
 
-// app.get('/hello', (req,res) => {//home page route
-//   res.send('Welcome to my pug page!'); //response sent back, what you see on the screen
-// });
-
-app.get('/', (req, res) => { //home page route
-	//response render index template with data passed in
-	res.render('index', {
-		data
-	})
+/*	ROUTES  */
+app.get('/', (req, res) => { //home page
+	//res.send('Welcome to my pug page!'); //response sent back, what you see on the screen
+	//render index template with data passed in from data.json
+	res.render('index', {data})
 })
 
-//about route
 app.get('/about', (req, res) => {
-	//render about
-	res.render('about')
+	res.render('about')//about.pug
 })
 
-app.get('/hello', (req,res) => {
-  res.render('project', {
-		data: data,
-		id: req.params.id
+app.get('/hello', (req, res) => {
+  res.render('project', {//project.pug
+		data: data,//something is going wrong here
+		id: req.params.id //request id
 	})
-  res.render('index'); //index.pug
-
 });
 
-//project route with req param of id
-// app.get('/project:id', (req, res) => {
-// 	res.render('project', {
-// 		data: data,
-// 		id: req.params.id
-// 	})
-// })
+//end ROUTES
+
+/*  ERRORS  */
+//404 error for routes that are not found
+app.use((req, res, next) => {
+	const error = new Error('Sorry, Page Not Found: Error Status --  ')
+	error.status = 404
+	next(error)
+});
+app.use((erro, req, res, next) => {
+	res.locals.error = erro
+	res.render('error') //error.pug
+})
+//end ERRORS
 
 app.listen(3000, () => {
-  console.log('App is running on localhost: 3000 ')
-});//3000 is the port
+  console.log('App is running on localhost port: 3000 ')
+})
