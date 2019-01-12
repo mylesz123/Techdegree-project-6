2b85192d8ab0c0f058bd8b76717f7d0b39ecbfbd
Myles Y <39877233+mylesz123@users.noreply.github.com>
2019-01-12 14:34:24 -0500

    get project.pug to work

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const express = require('express');
 const app = express();
 //require data from json file to fill in templates in project.pug file
 const data = require('./data.json');
-//nodemon so i dont have to keep going in the terminal
-//const nodemon = require('nodemon');
 
 //app.settings view engine for app to use pug templates
 app.set('view engine', 'pug');
@@ -12,10 +10,11 @@ app.set('view engine', 'pug');
 //.use public server to use static files (images, css)
 app.use('/static', express.static('public'));
 
-/*	ROUTES  */
+/********************/
+/******	ROUTES ******/
+/********************/
+
 app.get('/', (req, res) => { //home page
-	//res.send('Welcome to my pug page!'); //response sent back, what you see on the screen
-	//render index template with data passed in from data.json
 	res.render('index', {data})
 })
 
@@ -23,22 +22,25 @@ app.get('/about', (req, res) => {
 	res.render('about')//about.pug
 })
 
-app.get('/hello', (req, res) => {
+app.get('/:id', (req, res) => {
   res.render('project', {//project.pug
-		data: data,//something is going wrong here
+		//something is going wrong here
+		data,
 		id: req.params.id //request id
 	})
 });
 
-//end ROUTES
+/********************/
+/*****  ERRORS  *****/
+/********************/
 
-/*  ERRORS  */
 //404 error for routes that are not found
 app.use((req, res, next) => {
-	const error = new Error('Sorry, Page Not Found: Error Status --  ')
-	error.status = 404
-	next(error)
+	const erro = new Error('Sorry, Page Not Found ')
+	erro.status = 404
+	next(erro)
 });
+
 app.use((erro, req, res, next) => {
 	res.locals.error = erro
 	res.render('error') //error.pug
